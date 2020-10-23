2409d1c749cfcb332a18a5ac03307a19064faf7e
Jordan Banasik <woovie@woovie.net>
2020-10-23 13:36:56 -0700

    Reset camera to the player on resource stop

--- client.lua
+++ client.lua
@@ -89,3 +89,9 @@ end)
 reMap = function(value, low1, high1, low2, high2)
     return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
 end
+
+function resourceStop()
+    setCameraTarget(localPlayer)
+end
+
+addEventHandler("onClientResourceStop", resourceRoot, resourceStop)
