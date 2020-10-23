7b1ec1af0753b9d7b23b0c64d8aede53e74f43e2
Breno Danyel <38073795+iDannz-Breno@users.noreply.github.com>
2020-10-23 14:01:13 -0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,92 @@
+local progressUpdate = 5
+local refreshTick = 5
+
+local radius = 130
+
+local sW, sH = guiGetScreenSize()
+
+local imgSize = {
+    w = sH,
+    h = sH,
+}
+
+local texture = dxCreateTexture(imgSize.w, imgSize.h)
+local pixels = dxGetTexturePixels(texture)
+
+local boundX, boundY = 3300, 3500
+local x, y = boundX - radius, boundY
+
+Update = function()
+    local t = 0
+    while true do
+        if (y <= -boundY) then
+            y = boundY
+            x = x - (radius)
+        end
+
+        y = y - progressUpdate
+
+        for x2 = 0, radius do
+            local z = getGroundPosition(x + x2, y, 500) or 0
+            setCameraMatrix(x + x2, y, z + 400, x, y, z)
+
+            local color = {255, 255, 255}
+
+            if testLineAgainstWater(x + x2, y, z + 100, x + x2, y, z - 1) then
+                color = {115, 140, 220}
+            else
+                local ray, _, _, _, _, _, _, _, material = processLineOfSight(x + x2, y, z + 1, x + x2, y, z - 1, true, false, false, true, false,
+                                                                              false, false, false, localPlayer, false, false)
+                if ray and material then
+                    if materialColors[material] then
+                        color = materialColors[material]
+                    else
+                        outputChatBox(inspect({'not material', material}))
+                    end
+                end
+            end
+
+            local pX = reMap(x + x2, -boundY, boundX, 0, imgSize.w)
+            local pY = reMap(y, boundX, -boundY, 0, imgSize.h)
+
+            dxSetPixelColor(pixels, pX, pY, color[1], color[2], color[3])
+        end
+
+        if (x >= boundX) then
+            local pngPixels = dxConvertPixels(pixels, 'png')
+            local newImg = fileCreate('map.png')
+            fileWrite(newImg, pngPixels)
+            fileClose(newImg)
+
+            if isTimer(timer_C) then
+                killTimer(timer_C)
+            end
+            coroutine.yield()
+        end
+
+        t = t + 1
+        if t >= refreshTick then
+            coroutine.yield()
+            t = 0
+        end
+    end
+end
+
+updateC = coroutine.create(Update)
+timer_C = setTimer(function()
+    if coroutine.status(updateC) ~= 'dead' then
+        coroutine.resume(updateC)
+    end
+    dxSetTexturePixels(texture, pixels)
+end, refreshTick, 0)
+
+addEventHandler('onClientRender', root, function()
+    if texture then
+        dxDrawRectangle(sW / 2 - imgSize.w / 2, sH / 2 - imgSize.h / 2, imgSize.w, imgSize.h, tocolor(200, 200, 200, 50))
+        dxDrawImage(sW / 2 - imgSize.w / 2, sH / 2 - imgSize.h / 2, imgSize.w, imgSize.h, texture)
+    end
+end)
+
+reMap = function(value, low1, high1, low2, high2)
+    return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
+end
---
+++ config.lua
@@ -0,0 +1,219 @@
+materialColors = {}
+local r, g, b
+
+-- misc
+r, g, b = 255, 0, 0
+materialColors[48] = {r, g, b}
+materialColors[49] = {r, g, b}
+materialColors[60] = {r, g, b}
+materialColors[61] = {r, g, b}
+materialColors[62] = {r, g, b}
+materialColors[66] = {r, g, b}
+materialColors[67] = {r, g, b}
+materialColors[68] = {r, g, b}
+materialColors[71] = {r, g, b}
+materialColors[90] = {r, g, b}
+materialColors[91] = {r, g, b}
+materialColors[92] = {r, g, b}
+materialColors[93] = {r, g, b}
+materialColors[94] = {r, g, b}
+materialColors[95] = {r, g, b}
+materialColors[102] = {r, g, b}
+materialColors[103] = {r, g, b}
+materialColors[104] = {r, g, b}
+materialColors[105] = {r, g, b}
+materialColors[106] = {r, g, b}
+materialColors[107] = {r, g, b}
+materialColors[108] = {r, g, b}
+materialColors[158] = {r, g, b}
+materialColors[159] = {r, g, b}
+materialColors[163] = {r, g, b}
+materialColors[166] = {r, g, b}
+materialColors[169] = {r, g, b}
+materialColors[170] = {r, g, b}
+materialColors[176] = {r, g, b}
+materialColors[177] = {r, g, b}
+materialColors[178] = {r, g, b}
+
+-- water
+r, g, b = 115, 140, 220
+materialColors[38] = {r, g, b}
+materialColors[39] = {r, g, b}
+
+-- vegetation
+r, g, b = 122, 156, 20
+materialColors[23] = {r, g, b}
+materialColors[41] = {r, g, b}
+materialColors[111] = {r, g, b}
+materialColors[112] = {r, g, b}
+materialColors[113] = {r, g, b}
+materialColors[114] = {r, g, b}
+
+-- stone
+r, g, b = 100, 100, 100
+materialColors[18] = {r, g, b}
+materialColors[35] = {r, g, b}
+materialColors[36] = {r, g, b}
+materialColors[37] = {r, g, b}
+materialColors[69] = {r, g, b}
+materialColors[109] = {r, g, b}
+materialColors[154] = {r, g, b}
+materialColors[161] = {r, g, b}
+
+-- metal
+r, g, b = 160, 160, 160
+materialColors[50] = {r, g, b}
+materialColors[51] = {r, g, b}
+materialColors[52] = {r, g, b}
+materialColors[53] = {r, g, b}
+materialColors[54] = {r, g, b}
+materialColors[55] = {r, g, b}
+materialColors[56] = {r, g, b}
+materialColors[57] = {r, g, b}
+materialColors[58] = {r, g, b}
+materialColors[59] = {r, g, b}
+materialColors[63] = {r, g, b}
+materialColors[64] = {r, g, b}
+materialColors[65] = {r, g, b}
+materialColors[162] = {r, g, b}
+materialColors[164] = {r, g, b}
+materialColors[167] = {r, g, b}
+materialColors[168] = {r, g, b}
+materialColors[171] = {r, g, b}
+
+-- wood 
+r, g, b = 82, 65, 50
+materialColors[42] = {r, g, b}
+materialColors[43] = {r, g, b}
+materialColors[44] = {r, g, b}
+materialColors[70] = {r, g, b}
+materialColors[72] = {r, g, b}
+materialColors[73] = {r, g, b}
+materialColors[172] = {r, g, b}
+materialColors[173] = {r, g, b}
+materialColors[174] = {r, g, b}
+
+-- glass
+r, g, b = 163, 167, 177
+materialColors[45] = {r, g, b}
+materialColors[46] = {r, g, b}
+materialColors[47] = {r, g, b}
+materialColors[175] = {r, g, b}
+
+-- sand
+r, g, b = 232, 229, 151
+materialColors[28] = {r, g, b}
+materialColors[29] = {r, g, b}
+materialColors[30] = {r, g, b}
+materialColors[31] = {r, g, b}
+materialColors[32] = {r, g, b}
+materialColors[33] = {r, g, b}
+materialColors[74] = {r, g, b}
+materialColors[75] = {r, g, b}
+materialColors[76] = {r, g, b}
+materialColors[77] = {r, g, b}
+materialColors[78] = {r, g, b}
+materialColors[79] = {r, g, b}
+materialColors[86] = {r, g, b}
+materialColors[96] = {r, g, b}
+materialColors[97] = {r, g, b}
+materialColors[98] = {r, g, b}
+materialColors[99] = {r, g, b}
+materialColors[131] = {r, g, b}
+materialColors[143] = {r, g, b}
+materialColors[157] = {r, g, b}
+
+-- tierra 
+r, g, b = 125, 113, 90
+materialColors[19] = {r, g, b}
+materialColors[21] = {r, g, b}
+materialColors[22] = {r, g, b}
+materialColors[24] = {r, g, b}
+materialColors[25] = {r, g, b}
+materialColors[26] = {r, g, b}
+materialColors[27] = {r, g, b}
+materialColors[40] = {r, g, b}
+materialColors[83] = {r, g, b}
+materialColors[84] = {r, g, b}
+materialColors[87] = {r, g, b}
+materialColors[88] = {r, g, b}
+materialColors[100] = {r, g, b}
+materialColors[110] = {r, g, b}
+materialColors[123] = {r, g, b}
+materialColors[124] = {r, g, b}
+materialColors[126] = {r, g, b}
+materialColors[128] = {r, g, b}
+materialColors[129] = {r, g, b}
+materialColors[130] = {r, g, b}
+materialColors[132] = {r, g, b}
+materialColors[133] = {r, g, b}
+materialColors[141] = {r, g, b}
+materialColors[142] = {r, g, b}
+materialColors[145] = {r, g, b}
+materialColors[155] = {r, g, b}
+materialColors[156] = {r, g, b}
+
+-- grass (grama)
+r, g, b = 136, 186, 86
+materialColors[9] = {r, g, b}
+materialColors[10] = {r, g, b}
+materialColors[11] = {r, g, b}
+materialColors[12] = {r, g, b}
+materialColors[13] = {r, g, b}
+materialColors[14] = {r, g, b}
+materialColors[15] = {r, g, b}
+materialColors[16] = {r, g, b}
+materialColors[17] = {r, g, b}
+materialColors[20] = {r, g, b}
+materialColors[80] = {r, g, b}
+materialColors[81] = {r, g, b}
+materialColors[82] = {r, g, b}
+materialColors[115] = {r, g, b}
+materialColors[116] = {r, g, b}
+materialColors[117] = {r, g, b}
+materialColors[118] = {r, g, b}
+materialColors[119] = {r, g, b}
+materialColors[120] = {r, g, b}
+materialColors[121] = {r, g, b}
+materialColors[122] = {r, g, b}
+materialColors[125] = {r, g, b}
+materialColors[146] = {r, g, b}
+materialColors[147] = {r, g, b}
+materialColors[148] = {r, g, b}
+materialColors[149] = {r, g, b}
+materialColors[150] = {r, g, b}
+materialColors[151] = {r, g, b}
+materialColors[152] = {r, g, b}
+materialColors[153] = {r, g, b}
+materialColors[160] = {r, g, b}
+
+-- Concrete
+r, g, b = 60, 60, 60
+materialColors[4] = {r, g, b}
+materialColors[5] = {r, g, b}
+materialColors[7] = {r, g, b}
+materialColors[8] = {r, g, b}
+materialColors[34] = {r, g, b}
+materialColors[89] = {r, g, b}
+materialColors[127] = {r, g, b}
+materialColors[135] = {r, g, b}
+materialColors[136] = {r, g, b}
+materialColors[137] = {r, g, b}
+materialColors[138] = {r, g, b}
+materialColors[139] = {r, g, b}
+materialColors[144] = {r, g, b}
+materialColors[165] = {r, g, b}
+
+-- default 
+materialColors[0] = {100, 100, 100}
+materialColors[1] = {30, 30, 30}
+materialColors[2] = {20, 20, 20}
+materialColors[3] = {50, 50, 50}
+
+-- gravel
+r, g, b = 97, 90, 73
+materialColors[6] = {r, g, b}
+materialColors[85] = {r, g, b}
+materialColors[101] = {r, g, b}
+materialColors[134] = {r, g, b}
+materialColors[140] = {r, g, b}
